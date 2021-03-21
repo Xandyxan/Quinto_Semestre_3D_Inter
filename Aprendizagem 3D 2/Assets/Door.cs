e6a00b597acb40f970042c41e6b3b47878326a61
Xandyxan <79609879+Xandyxan@users.noreply.github.com>
2021-03-21 17:14:33 -0300

    Update Door.cs
    Changed line 30.

--- Aprendizagem 3D 2/Assets/Door.cs
+++ Aprendizagem 3D 2/Assets/Door.cs
@@ -27,6 +27,8 @@ public class Door : MonoBehaviour
 
     //Xandy> Testando merge conflict nos scripts! 16:51 - 21/03
 
+    //Xandy> Segundo teste de merge conflict nos scripts! 17:13 - 21/03
+
     // Start is called before the first frame update
     void Start()
     {
