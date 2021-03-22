05466bc57b3a437f054bb05117291966725244bb
GeorgeLGFreitas <georgelgfreitas@gmail.com>
2021-03-22 18:21:11 -0300

    Comentei o codigo de Inspeção/ Um problema encontrado
    Comentei o codigo de Inspeção para melhor entendimento.
    
    Percebi que se tiver mais de um objeto com o codigo de inspeção ele também vai sofrer o efeito de estar sendo inspecionado mesmo não sendo o objeto de origem selecionado.

--- Aprendizagem 3D 2/Assets/Scripts/Inspecao.cs
+++ Aprendizagem 3D 2/Assets/Scripts/Inspecao.cs
@@ -36,45 +36,48 @@ public class Inspecao : MonoBehaviour
         posicaoAtual = transform.position;
         rotacaoAtual = transform.rotation;
 
+        //Encerra o processo de inspeção
         if(Input.GetKeyDown(KeyCode.E))
         {
             sManager.inspecionando = false;
-            
-
         }
 
+        //O Obejto esta sendo inspecionado
         if(sManager.inspecionando)
         {
             
             posicaoAtual = Vector3.MoveTowards(posicaoAtual, Camera.main.transform.position, - Input.mouseScrollDelta.y * escala * Time.deltaTime);
-            //Colocar limite para Zoom de Inspeção
+            //Colocar limite para Zoom de Inspeção**
           
-            
-
+            //Rotaciona o obejto em inspeção quando o joghador aperta e segura o botão esquerdo do mouse e o move.
             if(Input.GetMouseButton(0))
             {
                 this.transform.Rotate((Input.GetAxis("Mouse Y") * rotVel * Time.deltaTime), (Input.GetAxis("Mouse X") * -rotVel * Time.deltaTime), 0, Space.World);
             }
+            //Rotaciona o objeto para a rotação inicial de inspeção quando o jogado não está mais pressionando o botão esquerdo do mouse
             else
             {
                
                 this.transform.rotation = Quaternion.Slerp(transform.rotation, pontoInspecao.rotation, Time.deltaTime * 2);
             }
             
-            transform.position = posicaoAtual;
+            this.transform.position = posicaoAtual;
         }
-        
-        if(posicaoAtual != origemPos && rotacaoAtual != origemRot && !sManager.inspecionando)
+
+        //Retorna o objeto para origem.
+        if(posicaoAtual != origemPos && rotacaoAtual != origemRot && !sManager.inspecionando) 
         {
             transform.position = Vector3.Slerp(posicaoAtual,origemPos, Time.deltaTime * 2);
             transform.rotation = Quaternion.Slerp(rotacaoAtual, origemRot, Time.deltaTime * 2);
         }
 
-        else if(posicaoAtual != pontoInspecao.position && rotacaoAtual != pontoInspecao.rotation && sManager.inspecionando && chegando)
+        //Leva o Objeto até o ponto de inspeção.
+        else if(posicaoAtual != pontoInspecao.position && rotacaoAtual != pontoInspecao.rotation && sManager.inspecionando && chegando) 
         {
             transform.position = Vector3.Slerp(posicaoAtual, pontoInspecao.position, Time.deltaTime * 8);
             transform.rotation =  Quaternion.Slerp(rotacaoAtual, pontoInspecao.rotation, Time.deltaTime * 8);
 
+            //Verifica a posição do objeto em relação ao ponto de inspeção e desliga o Slerp acima. 
             if((int)posicaoAtual.x == (int)pontoInspecao.position.x && (int)posicaoAtual.z == (int)pontoInspecao.position.z && (int)posicaoAtual.y == (int)pontoInspecao.position.y && 
             (int)rotacaoAtual.x == (int)pontoInspecao.rotation.x && (int)rotacaoAtual.y == (int)pontoInspecao.rotation.y && (int)rotacaoAtual.z == (int)pontoInspecao.rotation.z)
             
@@ -87,6 +90,7 @@ public class Inspecao : MonoBehaviour
 
     }
 
+    //Começa o processo de inspeção.    
     public void Interagindo()
     {
         chegando = true;
