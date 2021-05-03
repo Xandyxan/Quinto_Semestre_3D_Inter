ed02d76c34ba33452d5d22507b1bd245d8f151a2
Xandyxan <79609879+Xandyxan@users.noreply.github.com>
2021-05-03 20:59:59 -0300

    General Changes
    So many changes that I will not have time to explain, but summing up:
    
    • Tested several ways to have the best bake configurations for our needs
    
    • Prototype for the first scene
    
    • More detailed animations, character setup and more about the main character

---
+++ Aprendizagem 3D 2/Assets/PlayerVision.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerVision : MonoBehaviour
+{
+    [SerializeField] private Transform playerHead, cameraTransform, cameraOffSet;
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] bool lookCursor = true;
+    [SerializeField] [Range(0.0f, 0.5f)] float mouseSmoothTime = 0.3f;
+    private Vector2 cameraPitch = Vector2.zero;
+    [SerializeField] private Vector2 minPitch, maxPitch;
+
+    private Vector2 currentMouseDelta = Vector2.zero;
+    private Vector2 currentMouseVelocity = Vector2.zero;
+
+    [Header("Zoom")]
+    private Camera cameraPlayer;
+    [SerializeField] private Image crosshair;
+    [SerializeField] private float defaultZoomSpeed;
+    private float chRaio = 50f;
+
+    private float fovDefault = 90f;
+    private float fovZoom = 50f;
+    private float zoomSpeed = 8f;
+
+    private void Awake()
+    {
+        cameraPlayer = cameraTransform.GetComponent<Camera>();
+    }
+
+    void Start()
+    {
+        if (lookCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    void Update()
+    {
+        UpdateMouseLook();
+        HandleZoom();
+    }
+
+    private void UpdateMouseLook()
+    {
+        Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseVelocity, mouseSmoothTime);
+
+
+        cameraPitch += currentMouseDelta * mouseSensitivity;
+        cameraPitch.x = Mathf.Clamp(cameraPitch.x, minPitch.y, maxPitch.y);
+        cameraPitch.y = Mathf.Clamp(cameraPitch.y, minPitch.x, maxPitch.x);
+
+        //playerCamera.Rotate(Vector3.right * currentMouseDelta.y * mouseSensitivity + Vector3.up * currentMouseDelta.x * mouseSensitivity);
+        playerHead.localEulerAngles = Vector3.up * cameraPitch.x;
+        cameraTransform.localEulerAngles = Vector3.right * -cameraPitch.y;
+
+
+        cameraTransform.position = cameraOffSet.transform.position;
+        cameraTransform.localEulerAngles = new Vector3(cameraTransform.localEulerAngles.x, playerHead.localEulerAngles.y, 0);
+    }
+
+    private void HandleZoom()
+    {
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            cameraPlayer.fieldOfView = Mathf.Lerp(cameraPlayer.fieldOfView, fovZoom, defaultZoomSpeed * Time.deltaTime);
+            // make crosshair smaller.
+            crosshair.rectTransform.sizeDelta = Vector2.Lerp(crosshair.rectTransform.sizeDelta, new Vector2(chRaio / 2, chRaio / 2), zoomSpeed * Time.deltaTime);
+        }
+        else if (cameraPlayer.fieldOfView != fovDefault)
+        {
+            cameraPlayer.fieldOfView = Mathf.Lerp(cameraPlayer.fieldOfView, fovDefault, defaultZoomSpeed * Time.deltaTime);
+            // return crosshair to bigger size.
+            crosshair.rectTransform.sizeDelta = Vector2.Lerp(crosshair.rectTransform.sizeDelta, new Vector2(chRaio, chRaio), (zoomSpeed - 2) * Time.deltaTime);
+        }
+    }
+
+}
--- Aprendizagem 3D 2/Assets/Scripts/PlayerController.cs
+++ Aprendizagem 3D 2/Assets/Scripts/PlayerController.cs
@@ -23,6 +23,7 @@ public class PlayerController : MonoBehaviour
     float walkSpeedZ = 2.0f;
     float runSpeedZ = 4.0f;
     float backWalkSpeedZ = 1.25f;
+    float crouchSpeedZ = 1f;
 
     Vector2 currentDir = Vector2.zero;
     Vector2 currentDirVelocity = Vector2.zero;
@@ -30,7 +31,7 @@ public class PlayerController : MonoBehaviour
 
     Vector2 currentMouseDelta = Vector2.zero;
     Vector2 currentMouseVelocity = Vector2.zero;
-    [SerializeField] Transform head;
+    //[SerializeField] Transform head;
 
     [Header("Zoom")]
     [SerializeField] private Camera mainCamera;
@@ -42,9 +43,9 @@ public class PlayerController : MonoBehaviour
     private float fovZoom = 50f;
     private float zoomSpeed = 8f;
 
-    //run
+    //Character States
     private float fovRun = 80f;
-    private bool isRunning;
+    private bool isRunning, isCrouched;
 
     [Header("Other")]
     [SerializeField] SelectionManager SelectionManager;
@@ -57,7 +58,7 @@ public class PlayerController : MonoBehaviour
 
         actualWalkSpeedZ = walkSpeedZ;
     }
-    /*
+    
     // Start is called before the first frame update
     void Start()
     {   
@@ -67,13 +68,13 @@ public class PlayerController : MonoBehaviour
             Cursor.visible = false;
         }
     }
-    */
+    
     private void OnEnable()
     {
-        Cellphone.instance.usingCellphoneEvent -= TurnPlayerControllerOff; // we remove the methods from the delegate at the beggining to prevent it to run multiple times.
-        Cellphone.instance.closeCellMenuEvent -= TurnPlayerControllerOn;
-        Cellphone.instance.usingCellphoneEvent += TurnPlayerControllerOff;
-        Cellphone.instance.closeCellMenuEvent += TurnPlayerControllerOn;
+        //Cellphone.instance.usingCellphoneEvent -= TurnPlayerControllerOff; // we remove the methods from the delegate at the beggining to prevent it to run multiple times.
+        //Cellphone.instance.closeCellMenuEvent -= TurnPlayerControllerOn;
+        //Cellphone.instance.usingCellphoneEvent += TurnPlayerControllerOff;
+        //Cellphone.instance.closeCellMenuEvent += TurnPlayerControllerOn;
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -83,11 +84,16 @@ public class PlayerController : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
-       
+
     }
     // Update is called once per frame
     void Update()
     {
+        UpdateMouseLook();
+        UpdateMovement();
+        HandleZoom();
+
+        /*
         if (!usingCellphone)
         {
             if (!SelectionManager.inspecionando)
@@ -109,40 +115,33 @@ public class PlayerController : MonoBehaviour
                // print("PCtrl OF");
             }
         }
+        */
     }
 
     private void UpdateMouseLook()
     {
         Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseVelocity, mouseSmoothTime);
-        //print(currentMouseDelta);
         transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
         
 
         cameraPitch -= currentMouseDelta.y * mouseSensitivity;
         cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f);
         playerCamera.localEulerAngles = Vector3.right * cameraPitch;
-        head.localEulerAngles = Vector3.right * cameraPitch;
-        //playerCamera.Rotate(Vector3.right * -(mouseDelta.y * mouseSensitivity), Space.Self);
     }
 
     private void UpdateMovement()
     {
-        isRunning = false;
+        InputManager();
+
+        //Get RawMovement and normalize
         Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         targetDir.Normalize();
+        
+        //Pass raw values to make a smooth transtion
         currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
 
-        if (currentDir.y > -0.0001f && currentDir.y < 0.0001f) actualWalkSpeedZ = Mathf.Lerp(actualWalkSpeedZ, 0, Time.deltaTime * 10f);
-        else if (currentDir.y <= 0f) actualWalkSpeedZ = Mathf.Lerp(actualWalkSpeedZ, backWalkSpeedZ, Time.deltaTime * 10f);
-        else if (currentDir.y > 0f && !Input.GetButton("Run")) actualWalkSpeedZ = Mathf.Lerp(actualWalkSpeedZ, walkSpeedZ, Time.deltaTime * 10f);
-        else if (currentDir.y < 0f && actualWalkSpeedZ <= -1.4f) actualWalkSpeedZ = Mathf.Lerp(actualWalkSpeedZ, walkSpeedZ, Time.deltaTime * 1f);
-        else if (currentDir.y > -0.2f && Input.GetButton("Run"))
-        {
-            actualWalkSpeedZ = Mathf.Lerp(actualWalkSpeedZ, runSpeedZ, Time.deltaTime * 2f);
-            isRunning = true;
-        }
-
+        SmoothWalkSpeedZ();
 
         if (characterController.isGrounded)
             velocityY = 0.0f;
@@ -150,11 +149,68 @@ public class PlayerController : MonoBehaviour
         velocityY += gravity * Time.deltaTime;
 
         Vector3 velocity = (transform.forward * currentDir.y * actualWalkSpeedZ) + (transform.right * currentDir.x * walkSpeedX) + Vector3.up * velocityY;
-        animator.SetFloat("Velocity", Input.GetAxis("Vertical") * actualWalkSpeedZ);
-        //print(currentDir.y);
-
-        //When a GameObject is rotated, the blue arrow representing the Z axis of the GameObject also changes direction.
+        
         characterController.Move(velocity * Time.deltaTime);
+        SetAnimators();
+    }
+
+    private void InputManager()
+    {
+        if(Input.GetAxis("Horizontal") >= 0 || Input.GetAxis("Vertical") >=0)
+        {
+                UpdateCollider();
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            if (isCrouched) isCrouched = false;
+            else isCrouched = true;
+            isRunning = false;
+        }
+        
+
+        if (Input.GetButton("Run"))
+        {
+            isRunning = true;
+            isCrouched = false;
+        }
+        else isRunning = false;
+
+        if(Input.GetAxisRaw("Vertical") <= 0f) isRunning = false;
+    }
+
+    private void SmoothWalkSpeedZ() //It's to make velocity changes in a smooth way getting the smooth currentDir
+    {
+        if (!isCrouched && !isRunning)
+        {
+            if (currentDir.y > -0.0001f && currentDir.y < 0.0001f) //From any state to -> To idle stand state
+                actualWalkSpeedZ = Mathf.Lerp(actualWalkSpeedZ, 0, Time.deltaTime * 10f);
+
+            else if (currentDir.y <= 0f) //From any state to -> To walkingBack stand state
+                actualWalkSpeedZ = Mathf.Lerp(actualWalkSpeedZ, backWalkSpeedZ, Time.deltaTime * 10f);
+
+            else if (currentDir.y > 0f) //From any state to -> To walk forward stand state
+                actualWalkSpeedZ = Mathf.Lerp(actualWalkSpeedZ, walkSpeedZ, Time.deltaTime * 10f);
+        }
+        else if (isRunning && !isCrouched)
+        {
+            if (currentDir.y > 0f) //Se está correndo em pé
+                actualWalkSpeedZ = Mathf.Lerp(actualWalkSpeedZ, runSpeedZ, Time.deltaTime * 2f);
+        }
+        else if (isCrouched && !isRunning)
+        {
+            if (currentDir.y > -0.0001f && currentDir.y < 0.0001f) //From any state to -> To idle crouch state
+                actualWalkSpeedZ = Mathf.Lerp(actualWalkSpeedZ, 0, Time.deltaTime * 50f);
+
+            else if (currentDir.y > 0f && isCrouched && !isRunning) //From any state to -> To walk crouch state
+                actualWalkSpeedZ = Mathf.Lerp(actualWalkSpeedZ, crouchSpeedZ, Time.deltaTime * 50f);
+        }
+    }
+
+    private void SetAnimators()
+    {
+        animator.SetFloat("Velocity", Input.GetAxis("Vertical") * actualWalkSpeedZ);
+        animator.SetBool("isCrouched", isCrouched);
     }
 
     private void HandleZoom()
@@ -175,6 +231,22 @@ public class PlayerController : MonoBehaviour
             mainCamera.fieldOfView = Mathf.Lerp(mainCamera.fieldOfView, fovDefault, defaultZoomSpeed * Time.deltaTime);
             // return crosshair to bigger size.
             crosshair.rectTransform.sizeDelta = Vector2.Lerp(crosshair.rectTransform.sizeDelta, new Vector2(chRaio, chRaio), (zoomSpeed - 2) * Time.deltaTime);
+        }
+    }
+
+    private void UpdateCollider()
+    {
+        if(!isCrouched)
+        {
+            characterController.center = new Vector3(0, 0.0775f, 0);
+            characterController.radius = 0.03f;
+            characterController.height = 0.155f;
+        }
+        else
+        {
+            characterController.center = new Vector3(0, 0.0775f, 0.02f);
+            characterController.radius = 0.061f;
+            characterController.height = 0.155f;
         }
     }
 
