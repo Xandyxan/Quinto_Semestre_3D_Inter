5b6c2aa444d46fd738bb826b60e810c14caf5a3e
IgorPonciano <igorponciano.work@gmail.com>
2021-06-04 21:27:06 -0300

    Changes dia link
    - umas coisas ai

---
+++ Aprendizagem 3D 2/Assets/CheatMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheatMenu : MonoBehaviour
+{
+    [Header("SetSceneLoad")]
+    [SerializeField] int sceneIndex; // usado pra setar a cena pra ser carregada nos botões numericos do menu de confugurações / cheats
+
+    [Header("SetColetáveis")]
+    [SerializeField] string keycollectablePref;
+    [SerializeField] int prefValue;
+
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex); // setado pelo inspector 
+    }
+
+    public void SetItemKey(string itemKey)
+    {  // coloca a string do item
+        keycollectablePref = itemKey;
+    }
+
+    public void SetKeyValue(int itemValue)
+    { // seta o valor do pref, se for 1, o jogador coletou o item
+        prefValue = itemValue;
+    }
+
+    public void UpdateNewPref()
+    { // Chamar após setar os valores
+
+        PlayerPrefs.SetInt(keycollectablePref, prefValue);
+    }
+}
