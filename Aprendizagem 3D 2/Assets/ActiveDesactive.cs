cb21e05a44f3edc72507dae7fa79d647abb658af
IgorPonciano <igorponciano.work@gmail.com>
2021-06-05 21:51:05 -0300

    Sonho 1 Atualizado
    - Post Processing
    - modelos
    - Espelho

--- Aprendizagem 3D 2/Assets/ActiveDesactive.cs
+++ Aprendizagem 3D 2/Assets/ActiveDesactive.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class ActiveDesactive : MonoBehaviour
 {
+    [SerializeField] bool isMirror = false;
     private void Start()
     {
+        if(!isMirror)
         this.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
